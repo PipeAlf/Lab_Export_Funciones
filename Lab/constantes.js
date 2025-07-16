08602eee826e55c5c2ee77b3441736cfbe1a8578
PipeAlf <pipealf1996@gmail.com>
2025-07-16 14:43:29 -0500

    Ejercicio 1 y archivos .json y gitignore

---
+++ Lab/constantes.js
@@ -0,0 +1,3 @@
+export const PI = 3.1416;
+export const IVA = 0.19;
+export const DIAS_SEMANA = ['Lunes','Martes','Miercoles','Jueves','Viernes','Sábado','Domingo'];
---
+++ Lab/main.js
@@ -0,0 +1,36 @@
+import {PI,IVA} from "./constantes.js";
+
+//! Tarea 1 Ejercicio 1
+
+class areaCirc{
+    constructor(pi,radio){
+        this.pi = pi;
+        this.radio = radio;
+    }
+
+    calculoArea(){ 
+        let ar=0;
+        ar=this.pi*(this.radio*this.radio);
+        console.log("El área del circulo es de: ",ar," cm^2 ");
+    }
+}
+
+const area1 = new areaCirc(PI,2);
+area1.calculoArea();
+
+//! Tarea 2 Ejercicio 1
+
+class Precio{
+    constructor(precio,iva){
+        this.precio = precio;
+        this.iva = iva;
+    }
+    calculoIVA(){
+        let tot=0;
+        tot=(this.precio*this.iva)+this.precio;
+        console.log("El precio con IVA del producto es de: ", tot," USD.");
+    }
+}
+
+const precio1 = new Precio(50,IVA);
+precio1.calculoIVA();
